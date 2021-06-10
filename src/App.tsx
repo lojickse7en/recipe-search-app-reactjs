d9c794dd21793b42ff3562e71d80380f36e30858
lojickse7en <lojickse7en@BAYOs-SURFACE.localdomain>
2021-06-10 02:11:40 -0500

    updated async await

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,71 @@
-import React, { FormEvent, useEffect, useState } from 'react';
-// import logo from './logo.svg';
+import { RecordWithTtl } from 'dns';
+import React, {  FormEvent, useEffect, useState } from 'react';
 import './App.css';
-// import { StringifyOptions } from 'node:querystring';
-import { queryAllByAltText } from '@testing-library/dom';
 import { IRecipe } from './IRecipe';
-import Recipe from './RecipeComponent';
 import RecipeComponent from './RecipeComponent';
 
 function App() {
+  //sate to hold on to what data to be displyed to the user. What items hve been found when a search has been performed from the API
   const [recipesFound, setRecipesFound] = useState<IRecipe[]>([]);
+  
+  //for the input box
   const [recipeSearch, setRecipeSearch] = useState('');
+  
+  //calls out to the proxy api created
+  const searchForRecipes = async (query: string): Promise<IRecipe[]> => {
+    const result = await fetch(`http://localhost:3001/?search=${query}`)
+    // console.log(await result.text());
+    return (await result.json()).results;
+    };
+    
+    const search = (event: FormEvent<HTMLFormElement>) => {
+      //stops the form from submitting and reloading after the user submits the from
+      event.preventDefault();
 
-  const searchForRecipes = async (query: String): Promise<IRecipe[]> => {
-    const result = await fetch(`https://lojickse7en.github.io/recipe-search-app-reactjs/?search=${query}`)
-    const x = await fetch(`https://lojickse7en.github.io/recipe-search-app-reactjs/?search=${query}`)
-    console.log(await x.text());
-    return (await result.json());
-  };
+      //reference to the form
+      const form = event.target as HTMLFormElement;
+
+      //reference to the input
+      const input = form.querySelector('#searchText') as HTMLFormElement;
+      setRecipeSearch(input.value);
+    };
+
+
+
+   
 
   useEffect(() => {
     (async () => {
-      const query = encodeURIComponent(recipeSearch);
+      //if there is any special charatcters in the recipeSearch state
+      const query = encodeURIComponent(recipeSearch)
+      //conditional statement to only run the API query when user has done a search
+      if (query) {
+        //call thee searchforRecipies function we created and pass the query fucntion created
       const response = await searchForRecipes(query);
       setRecipesFound(response);
+      }
+      
     })();
   }, [recipeSearch]);
 
-  const search = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const input = form.querySelector('#searchText') as HTMLInputElement;
-    setRecipeSearch(input.value);
-    input.value = '';
-  };
 
   return (
     <div className="App">
       <h1>Recipe Search App</h1>
-      <form className="searchForm" onSubmit={event => search(event)} >
-        <input id="searchText" type="text" />
+      <form className='searchForm' onSubmit = {event => search(event)}>
+        <input id='searchText' type='text'/>
         <button>Search</button>
       </form>
-      {recipeSearch && <p>Results for {recipeSearch}...</p>}
-      <div className="recipes-container">
-        {recipesFound.length &&
+      {/* markup for component */}
+      {recipeSearch && <p>Results for { recipeSearch }...</p>}
+      <div className='recipe-container'>
+        {recipesFound.length && 
           recipesFound.map(recipe =>
-            (<RecipeComponent key={recipe.href} recipe={recipe}></RecipeComponent>))
-        }
+            (<RecipeComponent key={recipe.href} recipe={recipe}></RecipeComponent>)
+          )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
